1bee929e68ab509efa7b7ff83f1c96eb2a1d4c27
Ritesh kumar  jha <jharitesh548@gmail.com>
2024-10-25 00:25:23 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+
+document.querySelectorAll('.nav__links a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault(); 
+  
+      
+      const targetSection = document.querySelector(this.getAttribute('href'));
+  
+      
+      targetSection.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    });
+  });
+  
